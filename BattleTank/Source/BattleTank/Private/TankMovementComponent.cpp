629854360302d43a227e2478c56fa66ed122d5f3
Kvayn <sdorodko@gmail.com>
2020-01-27 17:14:37 +0200

    Complete moving forward and turning input implementation

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrack, UTankTrack* RightTrack)
+{
+	if (!LeftTrack && !RightTrack)
+	{
+		return;
+	}
+	this->LeftTrack = LeftTrack;
+	this->RightTrack = RightTrack;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend to move forward. Throw is %f"), Throw)
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurn(float Throw)
+{
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	//UE_LOG(LogTemp, Warning, TEXT("Intend to move forward. Throw is %f"), Throw)
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,6 +6,8 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+// Forward Declarations
+class UTankTrack;
 /**
  * 
  */
@@ -15,7 +17,18 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable, category = Setup)
+	void Initialize(UTankTrack* LeftTrack, UTankTrack* RightTrack);
 
 	UFUNCTION(BlueprintCallable, category = Input)
 	void IntendMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, category = Input)
+	void IntendTurn(float Throw);
+
+
+
+private:
+	UTankTrack* RightTrack = nullptr;
+	UTankTrack* LeftTrack = nullptr;
 };
